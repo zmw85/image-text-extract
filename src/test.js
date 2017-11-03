f7d4e09701c7ada77ceab24b54f1f216059e7b82
Zhengmin (Jeff) Wang <jeff.wang@infotrack.com.au>
2017-11-03 14:50:14 +1100

    x

--- src/test.js
+++ src/test.js
@@ -12,22 +12,29 @@ let googleVisionService = require('./services/googleVision');
 //   .then(res => {
 //     console.log(res);
 //   });
-  
-googleVisionService.textDetection('images/test.png')
+
+console.log('Processing...');
+googleVisionService.textDetection(process.argv[2] ? process.argv[2] : 'images/photo-id-1.jpg')
   .then(res => {
-    console.log(`extracted text: ${res}`);
+    // console.log(`extracted text: ${res}`);
 
     // let re = new RegExp('[0-9][0-9] [A-Z][A-Z][A-Z] [0-9][0-9][0-9][0-9]');
     const matches = res.match(/[0-9][0-9] [A-Z][A-Z][A-Z] [0-9][0-9][0-9][0-9]/g);
-    console.log(matches);
+    // console.log(matches);
 
     if (matches) {
       if (matches.length === 2) {
         const dateFormat = 'DD MMM YYYY';
-        const expiry = matches.map(m => moment(m, dateFormat)).sort().reverse()[0].format(dateFormat);
+        const expiry = matches.map(m => moment(m, dateFormat).format('YYYY-MM-DD'))
+          .sort()
+          .reverse()
+          .map(m => moment(m, 'YYYY-MM-DD'))[0]
+          .format(dateFormat);
         console.log(expiry);
       }
+    } else {
+      console.log("couldn't find expiry date");
     }
 
-    
   });
+    
